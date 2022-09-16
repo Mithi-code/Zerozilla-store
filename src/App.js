a1b5970334178fbc54ea2d0a924c40445d4d5bb3
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-09-16 12:06:51 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Home from './Components/Home';
 import Product from './Components/Product';
 import ProductDetails from './Components/ProductDetails';
 
+import React from "react";
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-     <Home />
-     <Product />
-     <ProductDetails />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path='/product/details' element={<ProductDetails/>}/>
+      </Routes>
     </div>
   );
 }
